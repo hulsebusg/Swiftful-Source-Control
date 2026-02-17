08d4247f1c530a5c2ce4a4879e770f99c65be525
Gary Hulsebus <hulsebusg@yahoo.com>
2026-02-17 15:27:57 -0800

    Initial Commit

---
+++ Swiftful Source Control/Swiftful_Source_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftful_Source_ControlApp.swift
+//  Swiftful Source Control
+//
+//  Created by Gary Hulsebus on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Swiftful_Source_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swiftful Source ControlTests/Swiftful_Source_ControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftful_Source_ControlTests.swift
+//  Swiftful Source ControlTests
+//
+//  Created by Gary Hulsebus on 2/17/26.
+//
+
+import Testing
+@testable import Swiftful_Source_Control
+
+struct Swiftful_Source_ControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
