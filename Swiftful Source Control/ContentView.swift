2610f9b6784bd419e21757d3a74da809ac5bf9f3
Gary Hulsebus <hulsebusg@yahoo.com>
2026-02-17 16:44:39 -0800

    another commit

--- Swiftful Source Control/ContentView.swift
+++ Swiftful Source Control/ContentView.swift
@@ -15,9 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.red)
             Text("Awe shucks")
             
-            Button("Click me") {
-                
-            }
+           
         }
         .padding()
     }
