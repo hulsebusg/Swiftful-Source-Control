f45d8543e6e364aa128980da7de093f9d4849ea9
Gary Hulsebus <hulsebusg@yahoo.com>
2026-02-18 18:53:37 -0800

    add home view

---
+++ Swiftful Source Control/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  Swiftful Source Control
+//
+//  Created by Gary Hulsebus on 2/18/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        
+        Text("Screen 1")
+        Text("Screen 2")
+        Text("Screen 3")
+        
+    }
+}
+
+#Preview {
+    HomeView()
+}
