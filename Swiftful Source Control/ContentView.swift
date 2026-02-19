6e736e3246f19ff23c82ae08c363b4dbc8b1a68b
Gary Hulsebus <hulsebusg@yahoo.com>
2026-02-18 19:27:18 -0800

    Added GIT NOTES

--- Swiftful Source Control/ContentView.swift
+++ Swiftful Source Control/ContentView.swift
@@ -4,6 +4,15 @@
 //
 //  Created by Gary Hulsebus on 2/17/26.
 //
+// GIT NOTES
+/*
+ Clone = Copy the repo to your personal computer
+ Commit = Save (locally) "Checkpoints" on your current branch
+ Stage = Prepare changes for commit
+ Stash = Save changes for later and revert to last Commit
+ Push = Send local commits to remote repo
+ Pull = Fetch remote commits to local repo
+ */
 
 import SwiftUI
 
@@ -15,7 +24,9 @@ struct ContentView: View {
                 .foregroundStyle(.red)
             Text("Awe shucks")
             
-           
+            Text("Hello, world!")
+            
+            Rectangle()
         }
         .padding()
     }
